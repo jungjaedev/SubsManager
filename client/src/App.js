8223fb2b6cbef4bd363ad3d38dc633961c39b4b8
jaehyun-jung <jaehyun.jung0326@gmail.com>
2021-12-30 13:13:33 +0000

    package mui to material-ui, template update

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import { Provider } from "react-redux";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+// import { StylesProvider } from "@material-ui/core/styles";
+
+import { store as mainStore } from "./app/store.js";
+import Structure from "./Structure";
+
+// function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.store = mainStore;
+  }
+  render() {
+    return (
+      <div className="App">
+        {/* <Provider store={this.store}> */}
+        {/* <StylesProvider>
+          <Provider store={this.store}> */}
+            <Structure />
+          {/* </Provider>
+        </StylesProvider> */}
+      </div>
+    );
+  }
 }
 
 export default App;

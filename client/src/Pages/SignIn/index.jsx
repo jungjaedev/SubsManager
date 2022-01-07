6deac71dccd58bcf2e173fb05590b5a136c95edd
Jaewon <jungjaedev@gmail.com>
2022-01-07 22:51:09 +0900

    Add: SignUp, SignIn, MyPage

--- client/src/Pages/SignIn/index.jsx
+++ client/src/Pages/SignIn/index.jsx
@@ -3,15 +3,72 @@ import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import { withTheme } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
 function SignIn(props) {
   const { classes } = props;
 
-  return <Box className={classes.root}></Box>;
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.root}>
+        <Box className={classes.row}>
+          <Grid>
+            <TextField id="standard-basic" label="email" />
+          </Grid>
+        </Box>
+        <Box className={classes.row}>
+          <Grid>
+            <TextField id="standard-basic" label="password" />
+          </Grid>
+        </Box>
+        <Box className={classes.row}>
+          <Grid item xs={8}></Grid>
+          <Grid item xs={4}>
+            <Typography variant="body1">로그인</Typography>
+          </Grid>
+        </Box>
+        <Box className={classes.row}>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={8}>
+            <Typography variant="body1">아이디가 없으신가요?</Typography>
+          </Grid>
+        </Box>
+        <Box className={classes.row}>
+          <Grid item xs={3}></Grid>
+          <Grid item xs={8}>
+            <Typography variant="body1">비밀번호가 기억나지 않으세요?</Typography>
+          </Grid>
+        </Box>
+      </Box>
+      <Box className={classes.root}>
+        <Box className={classes.row}>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={8}>
+            <Typography variant="body1">구글 로그인</Typography>
+          </Grid>
+        </Box>
+        <Box className={classes.row}>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={8}>
+            <Typography variant="body1">페이스북 로그인</Typography>
+          </Grid>
+        </Box>
+        <Box className={classes.row}>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={8}>
+            <Typography variant="body1">카카오톡 로그인</Typography>
+          </Grid>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 const componentStyle = withStyles(theme => ({
   root: theme.styles.Default.Box.root,
+  row: theme.styles.Default.Box.row,
 }));
 
 export default withTheme(componentStyle(SignIn));
--- client/src/Pages/index.jsx
+++ client/src/Pages/index.jsx
@@ -1,32 +1,38 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { withStyles } from "@material-ui/styles";
-import { withTheme } from "@material-ui/styles";
+import { withStyles } from '@material-ui/styles';
+import { withTheme } from '@material-ui/styles';
 
-import Box from "@material-ui/core/Box";
+import Box from '@material-ui/core/Box';
 
-import { activeMenu } from "../Data/manager";
+import { activeMenu } from '../Data/manager';
 
-import Main from "./Main";
-import ThisMonth from "./ThisMonth";
-import Statistics from "./Statistics";
-import Manage from "./Manage";
+import Main from './Main';
+import ThisMonth from './ThisMonth';
+import Statistics from './Statistics';
+import Manage from './Manage';
+import MyPage from './MyPage';
+// import SignIn from './SignIn';
+// import SignUp from './SignUp';
 
 function Pages(props) {
   const { classes } = props;
   const active = useSelector(activeMenu);
 
   const activeComponent = () => {
-    if (active === "thisMonth") return <ThisMonth />;
-    else if (active === "statistics") return <Statistics />;
-    else if (active === "manage") return <Manage />;
+    if (active === 'thisMonth') return <ThisMonth />;
+    else if (active === 'statistics') return <Statistics />;
+    else if (active === 'manage') return <Manage />;
+    else if (active === 'myPage') return <MyPage />;
     return <Main />;
+    // return <SignIn />;
+    // return <SignUp />;
   };
   return <Box className={classes.overflow}>{activeComponent}</Box>;
 }
 
-const componentStyle = withStyles((theme) => ({
+const componentStyle = withStyles(theme => ({
   overflow: theme.styles.Default.Box.overflow,
 }));
 
